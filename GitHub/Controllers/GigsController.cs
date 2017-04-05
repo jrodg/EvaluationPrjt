d370244afcb3ae5cbe33dd3f2a09c562ab455262
jrod.dev <jrod.201@hotmail.com>
2017-04-05 13:39:20 -0400

    Views, viewModel

---
+++ GitHub/Controllers/GigsController.cs
@@ -0,0 +1,25 @@
+﻿using GitHub.Models;
+using GitHub.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GitHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _ctx;
+        public GigsController()
+        {
+             _ctx = new ApplicationDbContext();
+        }
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _ctx.Genre.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
