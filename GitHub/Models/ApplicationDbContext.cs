7da8c96139ec2c1016e9d687cd37a181ed0a82c4
jrod.dev <jrod.201@hotmail.com>
2017-04-05 10:22:21 -0400

    Model changes

---
+++ GitHub/Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace GitHub.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Gig> Gig { get; set; }
+        public DbSet<Genre> Genre { get; set; }
+        public ApplicationDbContext()
+            : base("GitHub", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
--- GitHub/Models/ApplicationUser.cs
+++ GitHub/Models/ApplicationUser.cs
@@ -18,16 +18,5 @@ namespace GitHub.Models
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("GitHub", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-    }
+   
 }
