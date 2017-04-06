445cb3f91373998f2ef03e3c8a68540c0633a06b
jrod.dev <jrod.201@hotmail.com>
2017-04-06 17:18:22 -0400

    implement CSRF

--- GitHub/Controllers/GigsController.cs
+++ GitHub/Controllers/GigsController.cs
@@ -26,6 +26,7 @@ namespace GitHub.Controllers
         }
         [Authorize]
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(GigFormViewModel vm)
         {
             if (!ModelState.IsValid)
